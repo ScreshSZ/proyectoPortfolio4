5a5bed8f69c7d27605ccfe20e5bf5b4cbd4af65c
Santiago Zeniquel <santi_zeniquel_2016@hotmail.com>
2023-06-26 23:35:52 -0300

    se agregaron los archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnMobile = document.querySelector(".btnMobile")
+const linksNav = document.querySelector(".linksNav")
+
+btnMobile.addEventListener("click", ()=>{
+    
+    linksNav.classList.toggle("show")
+})
